60e35f8e419e0b34adb30537fd245c98637ef270
Emily Kelliher <kellihe_emil@bentley.edu>
2018-04-30 21:20:48 -0400

    post activity

---
+++ app/src/main/java/com/example/kellihe_emil/traveldiary/Post.java
@@ -0,0 +1,13 @@
+package com.example.kellihe_emil.traveldiary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Post extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
