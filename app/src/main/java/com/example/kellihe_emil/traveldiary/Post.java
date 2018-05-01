3e8412232b60e8a588dc90276dd85cf51759f40b
McKenna <buck_mcke@bentley.edu>
2018-05-01 16:44:45 -0400

    added notification

--- app/src/main/java/com/example/kellihe_emil/traveldiary/Post.java
+++ app/src/main/java/com/example/kellihe_emil/traveldiary/Post.java
@@ -6,6 +6,9 @@ package com.example.kellihe_emil.traveldiary;
 //Post Activity
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -26,6 +29,12 @@ public class Post extends Activity implements View.OnClickListener{
     private String file = "travel.txt";
     private String emailbody;
 
+    private NotificationManager mNotificationManager;
+    private Notification notifyDetails;
+    private int SIMPLE_NOTFICATION_ID;
+    private String contentText = "Your text has been created!";
+    private String tickerText = "New Alert, Click Me!";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +54,50 @@ public class Post extends Activity implements View.OnClickListener{
         t.schedule(task1, 1000);
         Timer t2 = new Timer();
         t2.schedule(task2, 5000);
+
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        //create intent for action when notification selected
+        //from expanded status bar
+        Intent notifyIntent = new Intent(this, TravelLog.class);
+
+		/*
+		  Intent notifyIntent = new Intent();
+		  notifyIntent.setComponent(new ComponentName("com.course.example",
+		                  "com.course.example.IOTest"));
+		 */
+
+        //create pending intent to wrap intent so that it
+        //will fire when notification selected.
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this, 0, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //build notification object and set parameters
+        notifyDetails =
+                new Notification.Builder(this)
+                        .setContentIntent(pendingIntent)
+
+                          //set Notification text and icon
+                        .setContentText(contentText)
+                        .setSmallIcon(R.drawable.droid)
+
+                        .setTicker(tickerText)            //set status bar text
+
+                        .setWhen(System.currentTimeMillis())    //timestamp when event occurs
+
+                        .setAutoCancel(true)     //cancel Notification after clicking on it
+
+                        //set Android to vibrate when notified
+                        .setVibrate(new long[]{1000, 1000, 1000, 1000})
+
+                        // flash LED (color, on in millisec, off)
+                        //doesn't work for all handsets
+                        .setLights(Integer.MAX_VALUE, 500, 500)
+
+                        .build();
+
+
+
     }
 
 
@@ -100,6 +153,8 @@ public class Post extends Activity implements View.OnClickListener{
         }
 
         }
+
+
     }
 
 
