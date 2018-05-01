b9cfe5d97e129eac75c2de9dbb9a6ffdde443814
Emily Kelliher <kellihe_emil@bentley.edu>
2018-04-30 21:20:24 -0400

    reviews activity

---
+++ app/src/main/java/com/example/kellihe_emil/traveldiary/Reviews.java
@@ -0,0 +1,17 @@
+package com.example.kellihe_emil.traveldiary;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.net.Uri;
+import android.content.Intent;
+
+// www.google.com/search?q= + text ********
+
+public class Reviews extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reviews);
+    }
+}
